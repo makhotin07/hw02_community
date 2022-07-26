95680af2197f193242dbe04367caafc6f52c010d
makhotin07 <108171268+makhotin07@users.noreply.github.com>
2022-07-26 15:06:37 +0300

    New commitment 2

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -2,7 +2,6 @@ from django.urls import include, path
 from django.contrib import admin
 
 
-
 urlpatterns = [
     path('', include('posts.urls', namespace='posts')),
     path('admin/', admin.site.urls),
