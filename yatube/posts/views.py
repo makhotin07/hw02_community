5efae0def74b57ec26d5154967a5b30b24b666c2
makhotin07 <108171268+makhotin07@users.noreply.github.com>
2022-07-27 18:51:32 +0300

    2 rewires

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import get_object_or_404, render
 
 from .models import Group, Post
 
+POSTS_PER_PAGE = 10
 
-# Create your views here.
+
 def index(request):
     template = 'posts/index.html'
-    posts = Post.objects.all()
+    posts = Post.objects.all()[:POSTS_PER_PAGE]
     context = {
         'posts': posts,
     }
@@ -16,7 +17,7 @@ def index(request):
 def group_posts(request, slug):
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group)
+    posts = Post.objects.filter(group=group)[:POSTS_PER_PAGE]
     context = {
         'group': group,
         'posts': posts
